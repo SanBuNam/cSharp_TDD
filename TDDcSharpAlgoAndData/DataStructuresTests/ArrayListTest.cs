6045f53de4d93ac22315ac5892afa33a3633883c
David Lee <david.lee@amwestfunding.com>
2021-11-03 17:25:29 -0700

    ArrayList and Testing

---
+++ TDDcSharpAlgoAndData/DataStructuresTests/ArrayListTest.cs
@@ -0,0 +1,35 @@
+﻿using DataStructures.Lists;
+using Xunit;
+
+namespace TDDcSharpAlgoAndData.DataStructuresTests
+{
+    // https://github.com/aalhour/C-Sharp-Algorithms
+    public static class ArrayListTest
+    {
+        [Fact]
+        public static void DoTest()
+        {
+            int index = 0;
+            var arrayList = new ArrayList<long>();
+
+            for (long i = 1; i < 1000000; ++i)
+            {
+                arrayList.Add(i);
+            }
+
+            for (int i = 1000; i < 1100; i++)
+            {
+                arrayList.RemoveAt(i);
+            }
+
+            for (int i = 100000; i < 100100; i++)
+            {
+                arrayList.Remove(i);
+            }
+
+
+
+
+        }
+    }
+}
