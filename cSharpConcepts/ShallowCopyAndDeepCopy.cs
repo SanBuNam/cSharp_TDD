e69a79761e9a9763fdbd2ef9b80f309148ad6fa1
David Lee <david.lee@amwestfunding.com>
2021-11-15 14:48:32 -0800

    Shallow Copy in C#

---
+++ cSharpConcepts/ShallowCopyAndDeepCopy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cSharpConcepts
+{
+    class ShallowCopyAndDeepCopy
+    {
+        // Understanding Shallow Copy in C#:
+        static void ShallowCopy(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.Name = "Anurag";
+            emp1.Department = "IT";
+            emp1.EmpAddress = new Address() { address = "BBSR" };
+            Employee emp2 = emp1.GetClone();
+            emp2.Name = "Pranaya";
+            emp2.EmpAddress.address = "Mumbai";
+            Console.WriteLine("Employee 1: ");
+            Console.WriteLine("Name: " + emp1.Name + ", Address: " + emp1.EmpAddress.address + ", Dept: " + emp1.Department);
+            Console.WriteLine("Employee 2: ");
+            Console.WriteLine("Name: " + emp2.Name + ", Address: " + emp2.EmpAddress.address + ", Dept: " + emp2.Department);
+            Console.Read();
+        }
+    }
+    // Employee 1: Name: Anurag, Address: Mumbai, Dept: IT
+    // Employee 2: Name: Pranaya, Address: Mumbai, Dept: IT
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public Address EmpAddress { get; set; }
+
+        public Employee GetClone()
+        {
+            return (Employee)this.MemberwiseClone();
+        }
+    }
+
+    public class Address
+    {
+        public string address { get; set; }
+    }
+}
