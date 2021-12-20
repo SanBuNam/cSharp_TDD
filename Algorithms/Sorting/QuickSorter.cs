e925788051311647045b36d1166719c017e2a739
David Lee <david.lee@amwestfunding.com>
2021-12-20 14:31:40 -0800

    selection sorter

--- Algorithms/Sorting/QuickSorter.cs
+++ Algorithms/Sorting/QuickSorter.cs
@@ -13,13 +13,11 @@ namespace Algorithms.Sorting
 
             // If the comparer is Null, then initialize it using a default typed comparer
             comparer = comparer ?? Comparer<T>.Default;
-
             collection.InternalQuickSort(startIndex, endIndex, comparer);
         }
 
         // Private static method
         // The recursive quick sort algorithm
-
         private static void InternalQuickSort<T>(this IList<T> collection, int leftmostIndex, int rightmostIndex, Comparer<T> comparer)
         {
             // Recursive call check
@@ -44,7 +42,7 @@ namespace Algorithms.Sorting
             // Compare remaining array elements against pivotValue
             wallIndex = leftmostIndex;
 
-            // Loop until pivot; exclusive!
+            // Loop until pivot: exclusive!
             for (int i = leftmostIndex; i <= (rightmostIndex - 1); i++)
             {
                 // check if collection[i] <= pivotValue
@@ -56,9 +54,8 @@ namespace Algorithms.Sorting
             }
 
             collection.Swap(wallIndex, pivotIndex);
-
             return wallIndex;
         }
-
     }
 }
+
