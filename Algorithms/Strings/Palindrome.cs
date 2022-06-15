4cd02740f781ca04eb7673569110f32d056065bc
David Lee <david.lee@amwestfunding.com>
2022-06-15 13:46:13 -0700

    Palindrome in c#

---
+++ Algorithms/Strings/Palindrome.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Algorithms.Strings
+{
+    public static class Palindrome
+    {
+        // check if palindrome
+        public static bool IsStringPalindrome(string word) => ToLowerAndTrim(word).Equals(ToLowerAndTrim(ReverseString(word)));
+
+        // string to lower and remove white space
+        private static string ToLowerAndTrim(string word) => Regex.Replace(word.ToLowerInvariant(), @"\s+", string.Empty);
+
+        private static string ReverseString(string word)
+        {
+            var arr = word.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
---
+++ TDDcSharpAlgoAndData/AlgorithmsTests/PalindromeTests.cs
@@ -0,0 +1,37 @@
+﻿using Algorithms.Strings;
+using Xunit;
+
+namespace UnitTest.AlgorithmsTests
+{
+    public static class PalindromeTests
+    {
+        public static void TextIsPalindrome_TrueExpected()
+        {
+            string test1 = "Anna";
+            string test2 = "A Santa at Nasa";
+
+            // Arrange
+            // Act
+            var isPalindrome = Palindrome.IsStringPalindrome(test1);
+            var isPalindrome2 = Palindrome.IsStringPalindrome(test2);
+            // Assert
+            Assert.True(isPalindrome);
+            Assert.True(isPalindrome2);
+        }
+
+
+        public static void TextNotPalindrome_FalseExpected()
+        {
+            string test1 = "hallo";
+            string test2 = "Once upon a time";
+
+            // Arrange
+            // Act
+            var isPalindrome = Palindrome.IsStringPalindrome(test1);
+            var isPalindrome2 = Palindrome.IsStringPalindrome(test2);
+            // Assert
+            Assert.False(isPalindrome);
+            Assert.False(isPalindrome2);
+        }
+    }
+}
