74f8ab5e7a0fcb1ce5fdeac1a08163d20e1a1f4b
David Lee <david.lee@amwestfunding.com>
2022-06-21 15:57:45 -0700

    String algorithm - Naive String Search

---
+++ Algorithms/Strings/NaiveStringSearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Algorithms.Strings
+{
+    /// <summary>
+    ///     Implements the traditional naive string matching algorithm in c#
+    /// </summary>
+    public static class NaiveStringSearch
+    {
+        /// <summary>
+        ///     NaiveSearch(Content, Pattern) will return an array containing each index of Content in which Pattern appears.
+        ///     Cost:  O(n*m).
+        /// </summary>
+        /// <param name="content">The text body across which to search for a given pattern.</param>
+        /// <param name="pattern">The pattern against which to check the given text body.</param>
+        /// <returns>Array containing each index of Content in which Pattern appears.</returns>
+        public static int[] NaiveSearch(string content, string pattern)
+        {
+            var m = pattern.Length;
+            var n = content.Length;
+            List<int> indices = new List<int>();
+            for (var e = 0; e <= n - m; e++)
+            {
+                int j;
+                for (j = 0; j < m; j++)
+                {
+                    if (content[e + j] != pattern[j])
+                    {
+                        break;
+                    }
+                }
+
+                if (j == m)
+                {
+                    indices.Add(e);
+                }
+            }
+
+            return indices.ToArray();
+        }
+    }
+}
---
+++ TDDcSharpAlgoAndData/AlgorithmsTests/NaiveStringSearchTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Algorithms.Strings;
+
+namespace TDDcSharpAlgoAndData.AlgorithmsTests
+{
+    public static class NaiveStringSearchTests
+    {
+        [Fact]
+        public static void ThreeMatchesFound_PassExpected()
+        {
+            var pattern = "ABB";
+            var content = "ABBBAAABBAABBBBAB";
+
+            var expectedOccurrences = new[] { 0, 6, 10 };
+            var actualOccurrences = NaiveStringSearch.NaiveSearch(content, pattern);
+            var sequencesAreEqual = expectedOccurrences.SequenceEqual(actualOccurrences);
+
+            Assert.True(sequencesAreEqual);
+        }
+
+
+        [Fact]
+        public static void OneMatchFound_PassExpected()
+        {
+            var pattern = "BAAB";
+            var content = "ABBBAAABBAABBBBAB";
+
+            var expectedOccurrences = new[] { 8 };
+            var actualOccurrences = NaiveStringSearch.NaiveSearch(content, pattern);
+            var sequencesAreEqual = expectedOccurrences.SequenceEqual(actualOccurrences);
+
+            Assert.True(sequencesAreEqual);
+        }
+
+        [Fact]
+        public static void NoMatchFound_PassExpected()
+        {
+            var pattern = "XYZ";
+            var content = "ABBBAAABBAABBBBAB";
+
+            var expectedOccurrences = new int[0];
+            var actualOccurrences = NaiveStringSearch.NaiveSearch(content, pattern);
+            var sequencesAreEqual = expectedOccurrences.SequenceEqual(actualOccurrences);
+
+            Assert.True(sequencesAreEqual);
+        }
+    }
+}
