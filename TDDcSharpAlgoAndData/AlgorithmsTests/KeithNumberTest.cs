72ae94ba1ad758422a24a8ad3c1c1a46a8868ac8
David Lee <david.lee@amwestfunding.com>
2022-06-22 16:38:37 -0700

    Keith number checker

---
+++ TDDcSharpAlgoAndData/AlgorithmsTests/KeithNumberTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Algorithms.Numeric;
+
+
+namespace TDDcSharpAlgoAndData.AlgorithmsTests
+{
+    public static class KeithNumberTest
+    {
+        [Theory]
+        [InlineData(14)]
+        [InlineData(47)]
+        [InlineData(197)]
+        [InlineData(7909)]
+        public static void KeithNumberWork(int number)
+        {
+            var result = KeithNumberChecker.IsKeithNumber(number);
+
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(-2)]
+        public static void KeithNumberShouldThrowEx(int number)
+        {
+            Assert.Throws<ArgumentException>(() => KeithNumberChecker.IsKeithNumber(number));
+        }
+
+    }
+}
