0b07c847649dbd24315a376ec7065e675afc157c
David Lee <david.lee@amwestfunding.com>
2022-10-21 12:18:14 -0700

    action delegate and dependency injection

---
+++ cSharpConcepts/ActionDelegate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cSharpConcepts
+{
+    public class FormalDelegate
+    {
+        /// <summary>
+        ///     Action is a delegate type defined in the System namespace.
+        ///     An Action type delegate is the same as Func delegate except that the Action delegate doesn't return a value. Like a void return type.
+        ///     An Action delegate can take up to 16 input parameters of different types.
+        /// </summary>
+        public delegate void Print(int val);
+        static void ConsolePrint(int i)
+        {
+            Console.Write(i);
+        }
+        static void Main(string[] vs)
+        {
+            Print prnt = ConsolePrint;
+            prnt(10);
+        }
+    }
+
+    // I can use an Action delegate instead of defining the above Print delegate, for example:
+    public class ActionDelegate
+    {
+        static void ConsolePrint2(int i)
+        {
+            Console.WriteLine(i);
+        }
+        static void Main(string[] vs)
+        {
+            // Action<int> printActionDel = ConsolePrint2;
+
+            // or I can initialize an Action delegate using the new keyword or by directly assigning a method:
+            Action<int> printActionDel = new Action<int>(ConsolePrint2);
+
+            printActionDel(10);
+        }
+    }
+
+    //  An Anonymous method can also be assigned to an Action delegate, for example:
+    public class AnonymousMethod
+    {
+        static void Main(string[] vs)
+        {
+            Action<int> printActionDel = delegate (int i)
+            {
+                Console.WriteLine(i);
+            };
+            printActionDel(10);
+        }
+    }
+
+    // A Lambda expression also can be used with an Action delegate:
+    public class LambdaExpress
+    {
+        static void Main(string[] stringps)
+        {
+            Action<int> printActionDel = i => Console.WriteLine(i);
+            printActionDel(10);
+        }
+    }
+
+
+}
---
+++ cSharpConcepts/DependencyInjection.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cSharpConcepts
+{
+    // Inversion of Control and Dependency Injection
+    // https://www.tutorialsteacher.com/ioc/dependency-injection
+    public class CustomerBusinessLogic
+    {
+        DataAccess _dataAccess;
+        public CustomerBusinessLogic()
+        {
+            _dataAccess = new DataAccess();
+        }
+        public string GetCustomerName(int id)
+        {
+            return _dataAccess.GetCustomerName(id);
+        }
+    }
+
+    public class DataAccess
+    {
+        public DataAccess() {}
+        public string GetCustomerName(int id)
+        {
+            return "Dummy Customer Name"; // get it from DB in real app
+        }
+    }
+    // Dependency Injection(DI) is a design pattern used to implement IoC.It allows the creation of dependent objects outside of a class and provides those objects to a class through different ways.Using DI, we move the creation and binding of the dependent objects outside of the class that depends on them.
+
+    // 
+    // DIP
+    public interface ICustomerDataAccess
+    {
+        string GetCustomerName(int id);
+    }
+    public class CustomerDataAccess : ICustomerDataAccess
+    {
+        public CustomerDataAccess() {}
+        public string GetCustomerName(int id)
+        {
+            return "Dummy Customer Name";
+        }
+    }
+    public class DataAccessFactory
+    {
+        public static ICustomerDataAccess GetCustomerDataAccessObj()
+        {
+            return new CustomerDataAccess();
+        }
+    }
+    public class CustomerBusinessLogic1
+    {
+        ICustomerDataAccess _custDataAccess;
+
+        public CustomerBusinessLogic1()
+        {
+            _custDataAccess = DataAccessFactory.GetCustomerDataAccessObj();
+        }
+
+        public string GetCustomerName(int id)
+        {
+            return _custDataAccess.GetCustomerName(id);
+        }
+    }
+    // The problem with the above example is that we used DataAccessFactory inside the CustomerBusinessLogic class. So, suppose there is another implementation of ICustomerDataAccess and we want to use that new class inside CustomerBusinessLogic. Then, we need to change the source code of the CustomerBusinessLogic class as well. The Dependency injection pattern solves this problem by injecting dependent objects via a constructor, a property, or an interface.
+
+    //
+    // eq. Constructor Injection
+    public interface ICustomerDataAccess2
+    {
+        string GetCustomerName2(int id);
+    }
+    public class CustomerDataAccess2 : ICustomerDataAccess2
+    {
+        public CustomerDataAccess2() { }
+        public string GetCustomerName2(int id)
+        {
+            // get the customer name from the db in real application
+            return "Dummy Customer Name";
+        }
+    }
+    
+    public class CustomerBusinessLogic2 
+    {
+        ICustomerDataAccess2 _dataAccess2;
+        public CustomerBusinessLogic2(ICustomerDataAccess2 custDataAccess2)
+        {
+            _dataAccess2 = custDataAccess2;
+        }
+        public CustomerBusinessLogic2()
+        {
+            _dataAccess2 = new CustomerDataAccess2();
+        }
+        public string ProcessCustomerData(int id)
+        {
+            return _dataAccess2.GetCustomerName2(id);
+        }
+    }
+    public class CustomerService
+    {
+        CustomerBusinessLogic2 _customerBL;
+        public CustomerService()
+        {
+            _customerBL = new CustomerBusinessLogic2(new CustomerDataAccess2());
+        }
+        public string GetCustomerName(int id)
+        {
+            return _customerBL.ProcessCustomerData(id);
+        }
+    }
+    // As you can see in the above example, the CustomerService class creates and injects the CustomerDataAccess object into the CustomerBusinessLogic class. Thus, the CustomerBusinessLogic class doesn't need to create an object of CustomerDataAccess using the new keyword or using factory class. The calling class (CustomerService) creates and sets the appropriate DataAccess class to the CustomerBusinessLogic class. In this way, the CustomerBusinessLogic and CustomerDataAccess classes become "more" loosely coupled classes.
+
+    //
+    // Property Injection
+    public class CustomerBusinessLogic3
+    {
+        public CustomerBusinessLogic3() {}
+        public string GetCustomerName(int id)
+        {
+            return DataAccess2.GetCustomerName2(id);
+        }
+        public ICustomerDataAccess2 DataAccess2 { get; set; }
+    }
+
+    public class CustomerService2
+    {
+        CustomerBusinessLogic3 _customerBL;
+        public CustomerService2()
+        {
+            _customerBL = new CustomerBusinessLogic3();
+            _customerBL.DataAccess2 = new CustomerDataAccess2();
+        }
+        public string GetCustomerName(int id)
+        {
+            return _customerBL.GetCustomerName(id);
+        }
+    }
+    // As you can see above, the CustomerBusinessLogic class includes the public property named DataAccess, where you can set an instance of a class that implements ICustomerDataAccess. So, CustomerService class creates and sets CustomerDataAccess class using this public property.
+    
+    //
+    // Method Injection
+    interface IDataAccessDependency
+    {
+        void SetDependency(ICustomerDataAccess2 customerDataAccess2);
+    }
+    public class CustomerBusinessLogic4 : IDataAccessDependency
+    {
+        ICustomerDataAccess2 _dataAccess2;
+        public CustomerBusinessLogic4() {}
+        public string GetCustomerName(int id)
+        {
+            return _dataAccess2.GetCustomerName2(id);
+        }
+        public void SetDependency(ICustomerDataAccess2 customerDataAccess2)
+        {
+            _dataAccess2 = customerDataAccess2;
+        }
+    }
+    public class CustomerService3
+    {
+        CustomerBusinessLogic4 _customerBL2;
+        public CustomerService3()
+        {
+            _customerBL2 = new CustomerBusinessLogic4();
+            ((IDataAccessDependency)_customerBL2).SetDependency(new CustomerDataAccess2());
+        }
+        public string GetCustomerName(int id)
+        {
+            return _customerBL2.GetCustomerName(id);
+        }
+    }
+    // In the above example, the CustomerBusinessLogic class implements the IDataAccessDependency interface, which includes the SetDependency() method. So, the injector class CustomerService will now use this method to inject the dependent class (CustomerDataAccess) to the client class.
+
+
+}
