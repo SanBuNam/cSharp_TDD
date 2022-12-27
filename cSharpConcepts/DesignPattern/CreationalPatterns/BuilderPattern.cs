45b97ed34476b1f122dc170e08c739dd726a539f
David Lee <david.lee@amwestfunding.com>
2022-12-27 15:47:17 -0800

    ProtoType design pattern.

--- cSharpConcepts/DesignPattern/CreationalPatterns/BuilderPattern.cs
+++ cSharpConcepts/DesignPattern/CreationalPatterns/BuilderPattern.cs
@@ -6,9 +6,12 @@ namespace cSharpConcepts.DesignPattern
 {
     class BuilderPattern
     {
-        // Builder is a creational design pattern, which allows constructing complex objects step by step.
-        // Unlike other creational patterns, Builder doesn't require products to have a common interface.
-        // That makes it possible to produce different products using the same construction process.
+        /*
+        Builder is a creational design pattern, which allows constructing complex objects step by step.
+        Unlike other creational patterns, Builder doesn't require products to have a common interface.
+        That makes it possible to produce different products using the same construction process.
+         */
+
         // The Builder interface specifies methods for creating the different parts of the product objects.
         public interface IBuilder
         {
