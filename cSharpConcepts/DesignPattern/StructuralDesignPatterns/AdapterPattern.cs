be09fff5fb3445f05863017a4ea9106e9c003033
David Lee <david.lee@amwestfunding.com>
2023-07-18 16:06:55 -0700

    Structural Adapter allows imcompatiable interfaces to work together

--- cSharpConcepts/DesignPattern/StructuralDesignPatterns/AdapterPattern.cs
+++ cSharpConcepts/DesignPattern/StructuralDesignPatterns/AdapterPattern.cs
@@ -60,4 +60,28 @@ namespace cSharpConcepts.DesignPattern.StructuralDesignPatterns
              */
         }
     }
+
+
+    // adapter #2 allows imcompatible interfaces to work together
+    class ToAdapt
+    {
+        public int Calculate(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    class IAdaptInterface
+    {
+        public void CalculateWithLessParameters(int a);
+    }
+
+    class Adapter : ToAdapt, IAdaptInterface
+    {
+        public int CalculateWithLessParameters(int a)
+        {
+            return Calculate(a, 10);
+        }
+    }
+
 }
